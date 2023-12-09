ec987656668fd13679ae0facf9e5cee43bbf4b95
leezan <san44583@gmail.com>
2023-12-09 15:21:00 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const str = "我有你祖国";
+</script>
+
+<template>
+  <div class="box">
+    <h1>我是App</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 400px;
+  height: 400px;
+  background-color: skyblue;
+  h1 {
+    color: red;
+  }
+}
+</style>
