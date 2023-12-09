522bfeaeed7841bc57501cb31e6ec81c077b873e
leezan <san44583@gmail.com>
2023-12-09 15:29:14 +0800

    使用husky提交的代码

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import {createApp} from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 
 // createApp(App).mount("#app");
 const app = createApp(App);
 app.mount("#app");
 
-const func = () => {
+const func =
 
 
-
-    console.log("hello world");
+    () => {
+  console.log("hello world");
 };
 
-
 func();
+
+
+const f = () => {
+
+
+
+  console.log("你为什么不自动格式化呢？");
+
+}
+
+f()
